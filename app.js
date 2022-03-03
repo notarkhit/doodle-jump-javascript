134b9fa0ac496e447ecd2b6e4432ba02000961ce
Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
2022-03-03 09:11:00 +0530

    Update app.js and style.css
    - Removing and adding new platforms using array work
    - Removing all children of the grid using while loop
    - added the score

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let isGoingRight = false
     let leftTimerId
     let rightTimerId
+    let score = 0
 
     function createDoodler () {
         grid.appendChild(doodler)
@@ -53,6 +54,16 @@ document.addEventListener('DOMContentLoaded', () => {
                 platform.bottom -= 4
                 let visual = platform.visual
                 visual.style.bottom = platform.bottom + 'px'
+
+                if (platform.bottom < 10){
+                    let firstPlatform = platforms[0].visual
+                    firstPlatform.classList.remove('platform')
+                    platforms.shift()
+                    score++ 
+                    console.log(platforms)
+                    let newPlatform = new Platform(600)
+                    platforms.push(newPlatform)
+                }
             })
         }
     }
@@ -98,8 +109,15 @@ document.addEventListener('DOMContentLoaded', () => {
     function gameOver() {
         console.log('game over')
         isGameOver = true
+        while (grid.firstChild) {
+            grid.removeChild(grid.firstChild)
+        }
+        grid.innerHTML = score
         clearInterval(upTimerId)
         clearInterval(downTimerId)
+        clearInterval(leftTimerId)
+        clearInterval(rightTimerId)
+        
     }
 
     function control(e){
@@ -125,7 +143,7 @@ document.addEventListener('DOMContentLoaded', () => {
             doodlerLeftSpace -= 5
             doodler.style.left = doodlerLeftSpace + 'px'
             } else moveRight()
-        },30)
+        },20)
     }
 
     function moveRight() {
@@ -139,7 +157,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 doodlerLeftSpace += 5
                 doodler.style.left = doodlerLeftSpace + 'px'
             }
-        },30)
+        },20)
     }
 
         function moveStraight(){
